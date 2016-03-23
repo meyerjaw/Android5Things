2fdd18d5f9bf60ff021b0ff07a91d74e40643571
Jason <meyerjaw@gmail.com>
2016-03-23 00:54:09 -0400

    Finished Swipe Tabs

---
+++ app/src/main/java/com/jakylcove/kanban.java
@@ -0,0 +1,19 @@
+package com.jakylcove;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jason on 3/22/2016.
+ */
+public class kanban extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.kanban2, container, false);
+    }
+
+}
